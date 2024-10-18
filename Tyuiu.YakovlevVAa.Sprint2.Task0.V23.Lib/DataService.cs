f819415058640a42f4204efaa9e91b16ac1e2ad8
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-10-18 15:35:32 +0500

    Task0

--- Tyuiu.YakovlevVAa.Sprint2.Task0.V23.Lib/DataService.cs
+++ Tyuiu.YakovlevVAa.Sprint2.Task0.V23.Lib/DataService.cs
@@ -6,11 +6,11 @@ namespace Tyuiu.YakovlevVAa.Sprint2.Task0.V23.Lib
         public bool[] GetCompareOperations(int x, int y)
         {
             bool[] res = new bool[6];
-            res[0] = x != y;
-            res[1] = x == y;
+            res[0] = x+690 == y;
+            res[1] = x+690 != y;
             res[2] = x < y;
-            res[3] = x <= y;
-            res[4] = x > y;
+            res[3] = x+691 > y;
+            res[4] = x+999 <= y;
             res[5] = x + 1000 >= y;
             return res;
         }
