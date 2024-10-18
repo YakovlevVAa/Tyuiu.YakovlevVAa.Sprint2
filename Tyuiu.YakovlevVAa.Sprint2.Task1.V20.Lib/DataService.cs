05415c4604c4c12f9b07a9ff9cc33fcb890d6d6a
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-10-18 16:40:59 +0500

    a

---
+++ Tyuiu.YakovlevVAa.Sprint2.Task1.V20.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.YakovlevVAa.Sprint2.Task1.V20.Lib
+{
+    public class DataService : ISprint2Task1V20
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a == b) | (b >= c);
+            res[1] = (a > d) & (c < d);
+            res[2] = (a >= c) || (c == d);
+            res[3] = (d < a) && (d <= b);
+            res[4] = !(b == a);
+            res[5] = (b < a) ^ (a <= d);
+            return res;
+        }
+    }
+}
--- Tyuiu.YakovlevVAa.Sprint2.Task1.V20/Program.cs
+++ Tyuiu.YakovlevVAa.Sprint2.Task1.V20/Program.cs
@@ -1,34 +1,40 @@
-﻿using Tyuiu.YakovlevVAa.Sprint2.Task0.V23.Lib;
-namespace Tyuiu.YakovlevVAa.Sprint2.Task0.V23
+﻿using Tyuiu.YakovlevVAa.Sprint2.Task1.V20.Lib;
+namespace Tyuiu.YakovlevVAa.Sprint2.Task1.V20
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            int x = 105;
-            int y = 795;
+            int a = 242;
+            int b = 155;
+            int c = 456;
+            int d = 17;
             bool[] res = new bool[2];
-            res = ds.GetCompareOperations(x, y);
+            res = ds.GetLogicOperations(a, b, c, d);
             Console.Title = "Спринт #2 | Выполнил: Яковлев В. А. | ИИПб-24-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #2                                                               *");
             Console.WriteLine("* Тема: Операции сравнения                                                *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
             Console.WriteLine("* Выполнил: Яковлев Виктор Александрович | ИИПб-24-2                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу из операций сравнений                                *");
-            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)*");
-            Console.WriteLine("* и арифметических выражений, которая вернет логическую                   *");
-            Console.WriteLine("* последовательность(массив): (True, False, True, True, False, True),     *");
-            Console.WriteLine("*  при x = 105, y = 795                                                   *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность можно чередовать, но          *");
+            Console.WriteLine("* использовать один раз в выражении) и логических операций                *");
+            Console.WriteLine("* (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),а*");
+            Console.WriteLine("* также арифметических выражений, которая вернет логическую               *");
+            Console.WriteLine("* последовательность(массив): (False, False, False, True, True, True),    *");
+            Console.WriteLine("* при a = 242, b = 155, c = 456, d = 17                                   *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("X =" + x);
-            Console.WriteLine("Y =" + y);
+            Console.WriteLine("A =" + a);
+            Console.WriteLine("B =" + b);
+            Console.WriteLine("C =" + c);
+            Console.WriteLine("D =" + d);
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
